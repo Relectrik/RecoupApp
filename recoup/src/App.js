914811c16f970cb6a0add1bd9153cb68599bff66
Relectrik <viv@dluvs.com>
2023-04-15 14:58:51 -0700

    loginpage prototype finished before firebase

--- recoup/src/App.js
+++ recoup/src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
 import logo from "./images/recouplogo.png";
+import googlelogo from "./images/googlelogo.png";
 
 function App() {
   return (
     <div className="LoginPage">
       <h1>Login Here</h1>
       <div className="LoginBox">
-        <div className="LoginInput"></div>
+        <div className="LoginInput">
+          <input className="EmailInput" placeholder="Email" />{" "}
+          <input className="PasswordInput" placeholder="Password" />
+          <button>Login</button>
+          <button>
+            <img src={googlelogo} alt="googlelogo" id="googlelogo"></img>
+            Login with Google
+          </button>
+        </div>
         <div className="Logo">
           <img src={logo} alt="logo" id="logoimg" />
         </div>
-
-        <a></a>
       </div>
     </div>
   );
